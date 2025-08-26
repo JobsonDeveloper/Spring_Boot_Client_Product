38a50e93858fd1d0d56bd1b00d88a0e8fbe6e490
Jobson de Oliveira Coutinho <jobsondeveloper@gmail.com>
2025-08-26 00:09:15 -0300

    Add files via upload

---
+++ SpringBoot_ClientProduct/ClienteService/src/main/java/br/com/jobson/compra/online/config/MongoConfig.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+package br.com.jobson.compra.online.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+
+/**
+ * @author jobson,oliveira
+ *
+ */
+@Configuration
+@EnableMongoRepositories(basePackages = "br.com.jobson.compra.online.repository")
+public class MongoConfig {
+
+}
